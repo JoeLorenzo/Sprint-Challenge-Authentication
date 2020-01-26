692fae9adaec9c3ba2b77efd89e63226d033eec8
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2020-01-25 21:06:40 -0600

    -Added JWT to register endpoint.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
 const User = require("./authUsersModels")
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
+function tokenGenerator(user){
+  const payload = {
+    username: user.name,
+    id: user.id
+  }
+  const options = {
+    expiresIn: '1h'
+  }
+  return jwt.sign(payload, process.env.JWT_SECRET_SAUCE || "43456Fdfw@$#", options)
+
+
+
+}
 router.post('/register', (req, res) => {
   const { username, password } = req.body 
   User.insert({ username, password: bcrypt.hashSync(password, 10) })
@@ -21,7 +35,8 @@ router.post('/login', (req, res) => {
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
         //console.log(user)
-        res.status(201).json({ message:"User successfully logged in!",  username})
+        const token = tokenGenerator(user)
+        res.status(201).json({ message:"User successfully logged in!",  token})
       }
       else {
         res.status(401).json({ message:"unauthorized" })
